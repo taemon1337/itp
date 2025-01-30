08b8ee9ae7f75cdbf3a036f7c8da61e5262fb78d
taemon1337 <timstello@gmail.com>
2025-01-30 08:21:22 -0500

    updated k8s.go certstore for cert-manager

--- pkg/certstore/k8s.go
+++ pkg/certstore/k8s.go
@@ -8,34 +8,50 @@ import (
 	"sync"
 	"time"
 
+	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	cmclient "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 )
 
-// K8sStore implements Store interface using Kubernetes TLS secrets
+// K8sStore implements Store interface using Kubernetes TLS secrets and cert-manager Certificates
 type K8sStore struct {
 	client        kubernetes.Interface
+	cmClient      cmclient.Interface
 	namespace     string
 	secretName    string
 	cache         map[string]*cachedCert
 	cacheMu       sync.RWMutex
 	cacheDuration time.Duration
 	defaultOpts   CertificateOptions
+	issuerRef     cmmeta.ObjectReference // Reference to the cert-manager issuer
 }
 
 // K8sOptions contains Kubernetes-specific store options
 type K8sOptions struct {
 	Options
-	Namespace  string
-	Client     kubernetes.Interface
-	CACertPEM []byte // Optional: CA certificate in PEM format
+	Namespace     string
+	Client        kubernetes.Interface
+	CMClient      cmclient.Interface
+	CACertPEM     []byte // Optional: CA certificate in PEM format
+	IssuerName    string // Name of the cert-manager issuer to use
+	IssuerKind    string // Kind of the cert-manager issuer (e.g., "ClusterIssuer" or "Issuer")
+	IssuerGroup   string // API group of the issuer (defaults to cert-manager.io)
 }
 
 // NewK8sStore creates a new Kubernetes-based certificate store
 func NewK8sStore(opts K8sOptions) *K8sStore {
+	if opts.IssuerGroup == "" {
+		opts.IssuerGroup = "cert-manager.io"
+	}
+
 	return &K8sStore{
 		client:        opts.Client,
+		cmClient:      opts.CMClient,
 		namespace:     opts.Namespace,
 		cache:        make(map[string]*cachedCert),
 		cacheDuration: opts.CacheDuration,
@@ -44,10 +60,19 @@ func NewK8sStore(opts K8sOptions) *K8sStore {
 			KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
 			ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
 		},
+		issuerRef: cmmeta.ObjectReference{
+			Name:  opts.IssuerName,
+			Kind:  opts.IssuerKind,
+			Group: opts.IssuerGroup,
+		},
 	}
 }
 
 func (s *K8sStore) GetCertificate(ctx context.Context, serverName string) (*tls.Certificate, error) {
+	return s.GetCertificateWithOptions(ctx, serverName, s.defaultOpts)
+}
+
+func (s *K8sStore) GetCertificateWithOptions(ctx context.Context, serverName string, opts CertificateOptions) (*tls.Certificate, error) {
 	// Check cache first
 	s.cacheMu.RLock()
 	if cached, ok := s.cache[serverName]; ok {
@@ -58,10 +83,28 @@ func (s *K8sStore) GetCertificate(ctx context.Context, serverName string) (*tls.
 	}
 	s.cacheMu.RUnlock()
 
-	// Fetch from Kubernetes
+	// Try to get existing secret
 	secret, err := s.client.CoreV1().Secrets(s.namespace).Get(ctx, serverName, metav1.GetOptions{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to get secret %s: %w", serverName, err)
+		if !errors.IsNotFound(err) {
+			return nil, fmt.Errorf("failed to get secret %s: %w", serverName, err)
+		}
+
+		// Secret doesn't exist, create or update Certificate resource
+		if err := s.createOrUpdateCertificate(ctx, serverName, opts); err != nil {
+			return nil, fmt.Errorf("failed to create/update certificate: %w", err)
+		}
+
+		// Wait for the certificate to be ready
+		if err := s.waitForCertificate(ctx, serverName); err != nil {
+			return nil, fmt.Errorf("timeout waiting for certificate: %w", err)
+		}
+
+		// Get the newly created secret
+		secret, err = s.client.CoreV1().Secrets(s.namespace).Get(ctx, serverName, metav1.GetOptions{})
+		if err != nil {
+			return nil, fmt.Errorf("failed to get created secret %s: %w", serverName, err)
+		}
 	}
 
 	cert, err := s.parseTLSSecret(secret)
@@ -80,9 +123,99 @@ func (s *K8sStore) GetCertificate(ctx context.Context, serverName string) (*tls.
 	return cert, nil
 }
 
-func (s *K8sStore) GetCertificateWithOptions(ctx context.Context, serverName string, opts CertificateOptions) (*tls.Certificate, error) {
-	// K8s store doesn't support custom options, fallback to default GetCertificate
-	return s.GetCertificate(ctx, serverName)
+// createOrUpdateCertificate creates or updates a cert-manager Certificate resource
+func (s *K8sStore) createOrUpdateCertificate(ctx context.Context, serverName string, opts CertificateOptions) error {
+	usages := []cmapi.KeyUsage{}
+	if opts.KeyUsage&x509.KeyUsageKeyEncipherment != 0 {
+		usages = append(usages, cmapi.UsageKeyEncipherment)
+	}
+	if opts.KeyUsage&x509.KeyUsageDigitalSignature != 0 {
+		usages = append(usages, cmapi.UsageDigitalSignature)
+	}
+	if opts.KeyUsage&x509.KeyUsageCertSign != 0 {
+		usages = append(usages, cmapi.UsageCertSign)
+	}
+
+	for _, usage := range opts.ExtKeyUsage {
+		switch usage {
+		case x509.ExtKeyUsageServerAuth:
+			usages = append(usages, cmapi.UsageServerAuth)
+		case x509.ExtKeyUsageClientAuth:
+			usages = append(usages, cmapi.UsageClientAuth)
+		}
+	}
+
+	cert := &cmapi.Certificate{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serverName,
+			Namespace: s.namespace,
+		},
+		Spec: cmapi.CertificateSpec{
+			SecretName: serverName,
+			CommonName: serverName,
+			Duration:   &metav1.Duration{Duration: opts.TTL},
+			IssuerRef:  s.issuerRef,
+			Usages:     usages,
+			PrivateKey: &cmapi.CertificatePrivateKey{
+				Algorithm: cmapi.RSAKeyAlgorithm,
+				Size:      2048,
+			},
+		},
+	}
+
+	// Add DNS names if specified
+	if len(opts.DNSNames) > 0 {
+		cert.Spec.DNSNames = opts.DNSNames
+	}
+
+	// Add IP addresses if specified
+	if len(opts.IPAddresses) > 0 {
+		ipStrings := make([]string, len(opts.IPAddresses))
+		for i, ip := range opts.IPAddresses {
+			ipStrings[i] = ip.String()
+		}
+		cert.Spec.IPAddresses = ipStrings
+	}
+
+	// Try to get existing certificate
+	existing, err := s.cmClient.CertmanagerV1().Certificates(s.namespace).Get(ctx, serverName, metav1.GetOptions{})
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			return fmt.Errorf("failed to get certificate: %w", err)
+		}
+		// Create new certificate
+		_, err = s.cmClient.CertmanagerV1().Certificates(s.namespace).Create(ctx, cert, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to create certificate: %w", err)
+		}
+	} else {
+		// Update existing certificate
+		existing.Spec = cert.Spec
+		_, err = s.cmClient.CertmanagerV1().Certificates(s.namespace).Update(ctx, existing, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to update certificate: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// waitForCertificate waits for the certificate to be ready
+func (s *K8sStore) waitForCertificate(ctx context.Context, name string) error {
+	return wait.PollImmediate(time.Second, 30*time.Second, func() (bool, error) {
+		cert, err := s.cmClient.CertmanagerV1().Certificates(s.namespace).Get(ctx, name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+
+		for _, cond := range cert.Status.Conditions {
+			if cond.Type == cmapi.CertificateConditionReady {
+				return cond.Status == cmmeta.ConditionTrue, nil
+			}
+		}
+
+		return false, nil
+	})
 }
 
 func (s *K8sStore) PutCertificate(ctx context.Context, serverName string, cert *tls.Certificate) error {
